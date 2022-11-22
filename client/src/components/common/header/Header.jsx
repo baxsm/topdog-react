43b2a5f60f20d9408845f347150a50ac300e991b
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-11-22 21:18:50 +0500

    Header update

--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Logo from '../logo/Logo'
+import { AiOutlineUser, AiOutlineHeart, AiOutlineShopping } from 'react-icons/ai'
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Header() {
 
@@ -8,11 +11,22 @@ export default function Header() {
         logoContainer: '',
         navigationContainer: '',
 
+        contentContainer: 'flex justify-center place-items-center gap-[2rem]',
         loginContainer: '',
         loginButtonContainer: 'text-[1.125rem]',
-        loginButton: '',
-        signupButton: '',
+        loginIcon: 'text-[1.25rem] cursor-pointer hover:text-secondary2Color',
+        favoriteContainer: '',
+        favoriteIcon: 'text-[1.25rem] cursor-pointer hover:text-secondary2Color',
+        cartContainer: 'relative',
+        cartButtonContainer: 'flex justify-center place-items-center gap-[0.5rem] rounded-full py-[8px] px-[15px] cursor-pointer gradientButton',
+        cartButtonIcon: 'text-[1.5rem]',
+        cartText: 'text-[1rem]',
+        cartDataContainer: 'absolute bg-[#fff] top-[4rem] right-[0rem] w-[350px] max-w-[350px] rounded-lg',
+        cartDataEmptyContainer: 'p-8 text-center',
+        cartDataEmptyText: 'text-[1rem] text-[#757575]',
     }
+
+    const [isOpenCartMenu, setIsOpenCartMenu] = useState(false)
 
     return (
         <header>
@@ -25,12 +39,27 @@ export default function Header() {
                         <li>Data</li>
                     </ul>
                 </div>
-                <div className={styles.loginContainer}>
-                    <div className={styles.loginButtonContainer}>
-                        <button className={styles.loginButton}>Sign in</button>
+                <div className={styles.contentContainer}>
+                    <div className={styles.loginContainer}>
+                        <Link to='/login'>
+                            <AiOutlineUser className={styles.loginIcon} />
+                        </Link>
                     </div>
-                    <div className={styles.loginButtonContainer}>
-                        <button className={styles.signupButton}>Sign up</button>
+                    <div className={styles.favoriteContainer}>
+                        <Link to='/favorites'>
+                            <AiOutlineHeart className={styles.favoriteIcon} />
+                        </Link>
+                    </div>
+                    <div className={styles.cartContainer}>
+                        <div className={styles.cartButtonContainer} onClick={() => isOpenCartMenu ? setIsOpenCartMenu(false) : setIsOpenCartMenu(true)}>
+                            <AiOutlineShopping className={styles.cartButtonIcon} />
+                            <p className={styles.cartText}>MY CART (0)</p>
+                        </div>
+                        <div className={isOpenCartMenu ? styles.cartDataContainer : 'hidden'}>
+                            <div className={styles.cartDataEmptyContainer}>
+                                <p className={styles.cartDataEmptyText}>Your cart is empty</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
