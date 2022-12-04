204cc5beff3ff1e13eeb9eea545d757e044662ca
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-12-04 18:32:54 +0500

    prisma using mysql

--- server/controllers/extra.js
+++ server/controllers/extra.js
@@ -2,6 +2,5 @@ import prisma from '../database/prisma.js'
 
 export const getBannerData = async (req, res) => {
     const data = await prisma.Banner.findMany({});
-    console.log('here');
     return res.status(200).json(data)
 }
